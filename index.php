581c15bccc044d6ecaa8715439beabc42d1750fe
Tobias Prein <tp199314@fh-muenster.de>
2014-10-15 22:51:41 +0200

    rough layout, first small design

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Breaking Point Spotting Tool</title>
+  <meta name="description" content="Spotting Tool for Breaking Point for better enemy spotting">
+  <meta name="author" content="Tobias Prein & Sebastian Haver">
+  <meta name="keywords" content="Spotting Tool Breaking Point CHAS">
+	
+  <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<div id="header">
+  <h1>CHAS Spotting Tool<h1>
+</div>
+<div id="content">
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+</div>
+<div id="footer">
+  <div id="footer_content">Developed by Brain and INKorgnito</div>
+</div>
+
+</body>
+</html>
